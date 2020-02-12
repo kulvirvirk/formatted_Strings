e61848c8d855714064abc601329a494f6127ff9b
kulvirvirk <kvirkdev@gmail.com>
2020-02-12 19:32:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# 1. print two strings 
+# 2. you escape sequences to print "" , new line, tab 
+# 3. concatenate both stings
+# 4. use len() to find length of new string
+# 5. print type() of new string
+# 6. format the string; combine string and int types 
+
+
+str1 = "Hello, how are you?"
+str2 = 'I\'m good!'
+age = 21
+location = 'California'
+
+print('string 1 is : ' + str1)
+print('string 2 is : ' + str2)
+
+print('\tstring 1 is : ' + str1)
+print('\nstring 2 is : ' + str2)
+
+str3 = str1 + ' ' + str2
+print(str3)
+
+print('This string\'s length is:' + str(len(str3)))
+
+print(type(str3))
+
+print(f'My age is {age} and I am from {location}!')
